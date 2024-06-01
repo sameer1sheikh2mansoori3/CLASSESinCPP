abe87bd203bf7fb5436db43c79377d67229a1c1d
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-06-01 10:48:29 +0530

    DEFUALT CLASS MEMEBERS ARE PRIVATE

---
+++ class.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int age;
+    // Student(name , age){
+    //     this.name = name;
+    //     this.age = age;
+    // }
+};
+int main(){
+    Student s;
+    s.name = "Sameer";
+    s.age = 12;
+    cout<<s.name<<endl;
+
+    return 0;
+
+}
